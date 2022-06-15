ba2f324a59be62251fbdd082fd4685a8b57e47b7
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2022-06-15 23:42:08 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
-const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.innerHTML = createGalleryItemsMarkup(galleryItems);
 
 function createGalleryItemsMarkup(images) {
   return images.map(({ preview, original, description }) => {
@@ -14,7 +14,7 @@ function createGalleryItemsMarkup(images) {
     </li>`}).join('');
 }
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 
-var lightBox = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay:250});
-lightBox.on('show.simplelightbox');
+// const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+// galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
